6e221247a091d90f79b009f817c46b3ce3548a42
HalcoDraco <pablohalcoguepard@gmail.com>
2025-12-05 11:56:10 +0100

    xd

---
+++ contracts/Box.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Box {
+    uint256 private _value;
+
+    event ValueChanged(uint256 value);
+
+    function store(uint256 value) public {
+        require(value > 0, "Value must be greater than 0");
+        _value = value;
+        emit ValueChanged(value);
+    }
+
+    function retrieve() public view returns (uint256) {
+        return _value;
+    }
+}
